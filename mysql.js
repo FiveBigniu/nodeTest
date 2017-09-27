ec1a8d15216f073af05ee84b1be0fa73cdae7708
Freed <flyxz@126.com>
2017-09-27 17:47:06 +0800

    frist

---
+++ mysql.js
@@ -0,0 +1,39 @@
+var mysql  = require('mysql');  
+ 
+var connection = mysql.createConnection({     
+  host     : 'localhost',       
+  user     : 'root',              
+  password : '',       
+  port: '3306',                   
+  database: 'sheila'
+}); 
+ 
+connection.connect(function(err){
+	if(err){
+		console.log('连接失败');
+	}else{
+		console.log('连接成功');
+		connection.query("CREATE TABLE person(id int,user varchar(255),password varchar(255))", function(err, result){
+			if(err) {
+				throw err
+			}else {
+				console.log('创建表成功');
+			}
+		})
+	}
+});
+ 
+// var  sql = 'SELECT * FROM websites';
+// //查
+// connection.query(sql,function (err, result) {
+//         if(err){
+//           console.log('[SELECT ERROR] - ',err.message);
+//           return;
+//         }
+ 
+//        console.log('--------------------------SELECT----------------------------');
+//        console.log(result);
+//        console.log('------------------------------------------------------------\n\n');  
+// });
+ 
+// connection.end();
